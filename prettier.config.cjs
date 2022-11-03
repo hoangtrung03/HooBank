b97273165ca039dc3d531d8d6736314206a8bbd3
vuhoangtrungbin0303 <hoangtrung@mobcec.com>
2022-11-03 16:50:37 +0700

    +add: setup source

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: "es5",
+  semi: false,
+  singleQuote: true,
+  arrowParens: "avoid",
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  tabWidth: 2,
+  trailingComma: "none",
+};
