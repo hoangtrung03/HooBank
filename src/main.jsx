4175333d2a6fcfe679ca98deb513365d89e70259
vuhoangtrungbin0303 <hoangtrung@mobcec.com>
2022-11-11 15:54:37 +0700

    +update: test i18n

--- src/main.jsx
+++ src/main.jsx
@@ -2,27 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-// import { I18nextProvider } from 'react-i18next'
-// import i18next from 'i18next'
-// import vi from './assets/i18n/vi.json'
-// import en from './assets/i18n/en.json'
+import { I18nextProvider } from 'react-i18next';
+import i18n from 'i18next';
+import vi from './assets/i18n/vi.json'
+import en from './assets/i18n/en.json'
 const root = ReactDOM.createRoot(document.getElementById('root'))
-// i18next.init({
-//   fallbackLng: 'en',
-//   interpolation: { escapeValue: false },
-//   resources: {
-//     en: {
-//       translation: en
-//     },
-//     vi: {
-//       translation: vi
-//     }
-//   }
-// })
+i18n.init({
+  fallbackLng: 'en',
+  interpolation: { escapeValue: false },
+  resources: {
+    en: {
+      translation: en
+    },
+    vi: {
+      translation: vi
+    }
+  }
+})
 root.render(
   <React.StrictMode>
-    {/* <I18nextProvider i18n={i18next}> */}
+    <I18nextProvider i18n={i18n}>
       <App />
-    {/* </I18nextProvider> */}
+    </I18nextProvider>
   </React.StrictMode>
 )
